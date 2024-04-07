fa3725653adddab9405b98954bf078879e72a4b5
Tanner Saint <tsaint2004@gmail.com>
2024-04-07 21:22:03 +0000

    added error handleing

--- main.c
+++ main.c
@@ -24,15 +24,15 @@ int main(int argc, char **argv)
 
     if (argc != 2)
     {
-        printf("wrong argument count");
-        return (1);
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
     }
 
     file = fopen(argv[1], "r");
     if (file == NULL)
     {
-        perror("Error opening file");
-        return (1);
+        fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
     }
 
     while (fgets(line, sizeof(line), file))
