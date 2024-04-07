691dacf5443a221186ede7a83a7e06418b19e066
Tanner Saint <tsaint2004@gmail.com>
2024-04-07 01:14:29 +0000

    added code to op_decode.c

--- op_decode.c
+++ op_decode.c
@@ -4,7 +4,19 @@
  *
 */
 
-void op_decode()
+void op_decode(char *ops[], instruction_t instruction[], stack_t **stack, unsigned int line_number)
 {
+    int i;
 
+    for (i = 0; i < sizeof(instruction)/sizeof(instruction[0]); i++)
+    {
+        if (strcmp(ops[0], instruction[i].opcode) == 0)
+        {
+            instruction[i].f(stack, line_number);
+            return;
+        }
+    }
+
+    printf("opcode not found");
+    return;
 }
