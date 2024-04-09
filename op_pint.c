10fe545840ee3fe479dba42e78880aae29810ebf
Tanner Saint <tsaint2004@gmail.com>
2024-04-09 03:57:45 +0000

    changed error message

--- op_pint.c
+++ op_pint.c
@@ -4,7 +4,7 @@ void pint(stack_t **stack, unsigned int line_number)
 {
     if (*stack == NULL)
     {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number + 1);
         exit(EXIT_FAILURE);
     }
     printf("%d\n", (*stack)->n);
